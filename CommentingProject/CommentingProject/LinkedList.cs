3c03d147f68cbc17e8296d761161c47156f7a452
Steven Burns <s.burns@baypath.net>
2017-10-05 12:00:38 -0400

    Fixed conflict of LinkedList - Steven

--- CommentingProject/CommentingProject/LinkedList.cs
+++ CommentingProject/CommentingProject/LinkedList.cs
@@ -7,24 +7,11 @@ using System.Threading.Tasks;
 namespace CommentingProject {
     class LinkedList {
         Node head;
-<<<<<<< HEAD
+
         public LinkedList(String value) { head = new Node(value, null); }
 
         public void comment(String value, Node previous) { previous.addNext(new Node(value, previous)); }
 
         public void remove(Node removal)  { if(removal == head) { head = null; } else { removal.getPrevious().getNext().Remove(removal) ; } }
-=======
-        public LinkedList() {
-            head = null;
-        }
-
-        public void post(String value) {
-            if(head == null) {
-                head = new Node(value, null);
-            } else {
-
-            }
-        }
->>>>>>> a39cd26d74d0b73915d598d37befa591665990e0
     }
 }
