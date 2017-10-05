64adad11e60036127627f0128efa8e7edc368525
Zachary Williams <z.williams@baypath.net>
2017-10-05 11:29:03 -0400

    Yeyah

--- CommentingProject/CommentingProject/LinkedList.cs
+++ CommentingProject/CommentingProject/LinkedList.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommentingProject
-{
-    class LinkedList
-    {
+namespace CommentingProject {
+    class LinkedList {
+        Node head;
+        public LinkedList() {
+            head = null;
+        }
+
+        public void post(String value) {
+            if(head == null) {
+                head = new Node(value, null);
+            } else {
+
+            }
+        }
     }
 }
