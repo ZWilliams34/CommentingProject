fab4ab29a93c43f43ff15b210f52c4b6236ea3c1
Steven Burns <s.burns@baypath.net>
2017-10-05 11:58:57 -0400

    Created comment and remove methods to LinkedList - Steven

--- CommentingProject/CommentingProject/LinkedList.cs
+++ CommentingProject/CommentingProject/LinkedList.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace CommentingProject {
     class LinkedList {
         Node head;
-        public LinkedList(String value) { head = new Node(value, null); } 
+        public LinkedList(String value) { head = new Node(value, null); }
+
+        public void comment(String value, Node previous) { previous.addNext(new Node(value, previous)); }
+
+        public void remove(Node removal)  { if(removal == head) { head = null; } else { removal.getPrevious().getNext().Remove(removal) ; } }
     }
 }
