12c08ba10fd4cef77d8b30ce657a738d6a69cfa9
Zachary Williams <z.williams@baypath.net>
2017-10-05 13:26:51 -0400

    Added form

--- CommentingProject/CommentingProject/Game1.cs
+++ CommentingProject/CommentingProject/Game1.cs
@@ -9,17 +9,13 @@ namespace CommentingProject {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Form1 form;
-        LinkedList[] posts;
-        int postsOnPage;
+        LinkedList oP;
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            posts = new LinkedList[5];
-            postsOnPage = 0;
+            oP = new LinkedList("Despite the constant negative press covfefe");
         }
-
-        public void post(String value) { if(postsOnPage < 5) { posts[postsOnPage] = new LinkedList(value); } }
 
         protected override void Initialize() {
             form = new Form1();
