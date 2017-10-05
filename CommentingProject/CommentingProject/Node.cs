04d9449b362d9bee7dba0e0b57220f80e50542b1
Steven Burns <s.burns@baypath.net>
2017-10-05 11:25:27 -0400

    Created and added part of post() to LinkedList.cs - Steven

--- CommentingProject/CommentingProject/Node.cs
+++ CommentingProject/CommentingProject/Node.cs
@@ -4,16 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommentingProject
-{
-    class Node
-    {
+namespace CommentingProject {
+    class Node {
         private Node[,] next;
         private Node previous;
         private String value;
 
-        public Node(String valueus, Node previousus, Node[,] nextus)
-        {
+        public Node(String valueus, Node previousus, Node[,] nextus) {
             value = valueus;
             previous = previousus;
             next = nextus;
