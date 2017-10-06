80832a34f4521b6596a6124b66eb7e374135dc0e
Zachary Williams <z.williams@baypath.net>
2017-10-06 08:14:23 -0400

    ayyy

--- CommentingProject/CommentingProject/Form1.cs
+++ CommentingProject/CommentingProject/Form1.cs
@@ -11,30 +11,35 @@ using System.Windows.Forms;
 namespace CommentingProject {
     public partial class Form1 : Form {
         public String enteredAuthor, enteredContent;
-<<<<<<< HEAD
         public Form1()
         {
             InitializeComponent();
             enteredAuthor = "";
             enteredContent = "";
         }
-=======
->>>>>>> 55fcf526f627c69729b6bc086e8c621fb4ebf0ad
-
-        public Form1() { InitializeComponent(); }
 
         private void Form1_Load(object sender, EventArgs e) { }
 
-        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            enteredAuthor = e.ToString();
+        }
 
-        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            enteredContent = e.ToString();
+        }
 
         private void button1_Click(object sender, EventArgs e) { }
 
-        private void button2_Click(object sender, EventArgs e) { }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            enteredContent = "";
+            enteredAuthor = "";
+        }
 
-        private String getAuthor() { return "a"; }
+        private String getAuthor() { return enteredAuthor; }
 
-        private String getValue() { return "a"; }
+        private String getValue() { return enteredContent; }
     }
 }
