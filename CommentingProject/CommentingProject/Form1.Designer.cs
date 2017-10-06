c0ed7dfe5867721d1280f51c70ec59ae9d191b2a
Steven Burns <s.burns@baypath.net>
2017-10-06 08:03:30 -0400

    Removed label methods for form and did some formatting - Steven

--- CommentingProject/CommentingProject/Form1.Designer.cs
+++ CommentingProject/CommentingProject/Form1.Designer.cs
@@ -59,25 +59,6 @@
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 48);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Author";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 111);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Post";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(44, 136);
@@ -99,7 +80,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Comment";
+            this.Text = "Post";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- CommentingProject/CommentingProject/Form1.cs
+++ CommentingProject/CommentingProject/Form1.cs
@@ -11,39 +11,16 @@ using System.Windows.Forms;
 namespace CommentingProject {
     public partial class Form1 : Form {
         public String enteredAuthor;
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        public Form1() { InitializeComponent(); }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) { }
 
-        }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
 
-        }
+        private void button1_Click(object sender, EventArgs e) { }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            enteredAuthor = e.ToString();
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
+        private void button2_Click(object sender, EventArgs e) { }
     }
 }
